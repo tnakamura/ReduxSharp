6aa71fa6b9f0c3de5a583b74df64892849161157
tnakamura <griefworker@gmail.com>
2016-09-20 14:45:25 +0900

    Wrote the XML comment of the IStore<TState> interface

--- src/ReduxSharp/IStore.cs
+++ src/ReduxSharp/IStore.cs
@@ -2,10 +2,27 @@
 
 namespace ReduxSharp
 {
+    /// <summary>
+    /// Represents a store that holds the complete state tree of your application.
+    /// </summary>
+    /// <typeparam name="TState">A type of root state tree</typeparam>
     public interface IStore<TState> : IObservable<TState>
     {
+        /// <summary>
+        /// Returns the current state tree of your application.
+        /// </summary>
         TState State { get; }
 
+        /// <summary>
+        /// Dispatches an action.
+        /// </summary>
+        /// <remarks>
+        /// This is the only way to trigger a state change.
+        /// </remarks>
+        /// <param name="action">
+        /// An object describing the change that makes sense for your application.
+        /// </param>
+        /// <returns>The dispatched action</returns>
         IAction Dispatch(IAction action);
     }
 }
