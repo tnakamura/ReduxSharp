cdf12ee62faf4966bfc797b3d5a67d14f75e7f31
tnakamura <griefworker@gmail.com>
2016-09-20 14:52:24 +0900

    Wrote the XML comment of the IReducer<TState> interface

--- src/ReduxSharp/IReducer.cs
+++ src/ReduxSharp/IReducer.cs
@@ -1,7 +1,17 @@
 ﻿namespace ReduxSharp
 {
+    /// <summary>
+    /// Represents a reducing object.
+    /// </summary>
+    /// <typeparam name="TState">A type of state tree</typeparam>
     public interface IReducer<TState>
     {
+        /// <summary>
+        /// Accepts an accumulation and a value and returns a new accumulation.
+        /// </summary>
+        /// <param name="state">A state object</param>
+        /// <param name="action">An action object</param>
+        /// <returns>A new state object</returns>
         TState Invoke(TState state, IAction action);
     }
 }
