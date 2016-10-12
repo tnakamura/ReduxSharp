c4f8a58e25b3ea2249aa9e5a2c1a69138e8bb7be
tnakamura <griefworker@gmail.com>
2016-10-12 13:57:29 +0900

    Add test project

---
+++ test/ReduxSharp.Tests/StoreBuilderTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ReduxSharp.Tests
+{
+    public class StoreBuilderTest
+    {
+        public class AppState { }
+
+        public class AppReducer : IReducer<AppState>
+        {
+            public AppState Invoke(AppState state, IAction action)
+            {
+                return state ?? new AppState();
+            }
+        }
+
+        [Fact]
+        public void Constructor_initialize_new_instance()
+        {
+            var builder = new StoreBuilder<AppState>(new AppReducer());
+            Assert.NotNull(builder);
+        }
+
+        [Fact]
+        public void Constructor_throws_ArgumentNullException_when_reducer_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new StoreBuilder<AppState>(null);
+            });
+        }
+
+        [Fact]
+        public void Build_returns_new_store_instance()
+        {
+            var builder = new StoreBuilder<AppState>(new AppReducer());
+            var store = builder.Build();
+            Assert.NotNull(store);
+        }
+    }
+}
---
+++ test/ReduxSharp.Tests/StoreTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace ReduxSharp.Tests
+{
+    public class StoreTest
+    {
+        public class AppState
+        {
+        }
+
+        public class AppReducer : IReducer<AppState>
+        {
+            public AppState Invoke(AppState state, IAction action)
+            {
+                return state ?? new AppState();
+            }
+        }
+
+        [Fact]
+        public void Constructor_initialize_new_instance()
+        {
+            var store = new Store<AppState>(new AppReducer());
+            Assert.NotNull(store);
+        }
+
+        [Fact]
+        public void Constructor_throws_ArgumentNullException_when_reducer_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new Store<AppState>(null);
+            });
+        }
+
+        [Fact]
+        public void Dispatch_throws_ArgumentNullException_when_action_is_null()
+        {
+            var store = new Store<AppState>(new AppReducer());
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                store.Dispatch(null);
+            });
+        }
+    }
+}
