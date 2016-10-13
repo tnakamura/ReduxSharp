5609381692cb8703c3ab6f8323cd9b3f5a9e0eb4
tnakamura <griefworker@gmail.com>
2016-10-13 12:10:58 +0900

    Refactoring

---
+++ src/ReduxSharp/Internal/MiddlewareFactory.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ReduxSharp.Internal
+{
+    internal static class MiddlewareFactory
+    {
+        private const string InvokeMethodName = "Invoke";
+
+        public static MiddlewareDelegate<TState> Create<TState>(Type middleware, object[] args)
+        {
+            return (store, next) =>
+            {
+                var typeInfo = middleware.GetTypeInfo();
+
+                var invokeMethod = typeInfo.GetDeclaredMethod(InvokeMethodName);
+                if (invokeMethod == null)
+                {
+                    throw new InvalidOperationException($"{middleware.Name} require {InvokeMethodName} method");
+                }
+
+                var invokeParameters = invokeMethod.GetParameters();
+                if (invokeParameters.Length == 0)
+                {
+                    throw new InvalidOperationException($"{InvokeMethodName} required action argument");
+                }
+
+                var ctorArgs = new object[args.Length + 2];
+                ctorArgs[0] = store;
+                ctorArgs[1] = next;
+                Array.Copy(args, 0, ctorArgs, 2, args.Length);
+                var instance = Activator.CreateInstance(middleware, ctorArgs);
+
+                var factory = Compile<object>(invokeMethod, invokeParameters);
+
+                return action =>
+                {
+                    return factory(instance, action);
+                };
+            };
+        }
+
+        private static Func<T, IAction, IAction> Compile<T>(MethodInfo methodInfo, ParameterInfo[] parameters)
+        {
+            var middleware = typeof(T);
+            var actionArg = Expression.Parameter(typeof(IAction), "action");
+            var instanceArg = Expression.Parameter(middleware, "instance");
+
+            // Invoke method's argument is action only.
+            var methodArguments = new Expression[parameters.Length];
+            methodArguments[0] = actionArg;
+
+            Expression middleweareInstanceArg = instanceArg;
+            if (methodInfo.DeclaringType != typeof(T))
+            {
+                middleweareInstanceArg = Expression.Convert(middleweareInstanceArg, methodInfo.DeclaringType);
+            }
+
+            var body = Expression.Call(middleweareInstanceArg, methodInfo, methodArguments);
+
+            var lambda = Expression.Lambda<Func<T, IAction, IAction>>(body, instanceArg, actionArg);
+
+            return lambda.Compile();
+        }
+    }
+}
--- src/ReduxSharp/StoreBuilder.cs
+++ src/ReduxSharp/StoreBuilder.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using ReduxSharp.Internal;
 
 namespace ReduxSharp
 {
@@ -69,8 +70,6 @@ namespace ReduxSharp
             return new Store<TState>(_reducer, _initialState, _middlewares.ToArray());
         }
 
-        private const string InvokeMethodName = "Invoke";
-
         /// <summary>
         /// Adds a middleware type to the store's dispatch pipeline.
         /// </summary>
@@ -90,58 +89,9 @@ namespace ReduxSharp
         /// <returns>The <see cref="IStoreBuilder{TState}"/> instance.</returns>
         public IStoreBuilder<TState> UseMiddleware(Type middleware, params object[] args)
         {
-            return UseMiddleware((store, next) =>
-            {
-                var typeInfo = middleware.GetTypeInfo();
+            if (middleware == null) throw new ArgumentNullException(nameof(middleware));
 
-                var invokeMethod = typeInfo.GetDeclaredMethod(InvokeMethodName);
-                if (invokeMethod == null)
-                {
-                    throw new InvalidOperationException($"{middleware.Name} require {InvokeMethodName} method");
-                }
-
-                var invokeParameters = invokeMethod.GetParameters();
-                if (invokeParameters.Length == 0)
-                {
-                    throw new InvalidOperationException($"{InvokeMethodName} required action argument");
-                }
-
-                var ctorArgs = new object[args.Length + 2];
-                ctorArgs[0] = store;
-                ctorArgs[1] = next;
-                Array.Copy(args, 0, ctorArgs, 2, args.Length);
-                var instance = Activator.CreateInstance(middleware, ctorArgs);
-
-                var factory = Compile<object>(invokeMethod, invokeParameters);
-
-                return action =>
-                {
-                    return factory(instance, action);
-                };
-            });
-        }
-
-        private static Func<T, IAction, IAction> Compile<T>(MethodInfo methodInfo, ParameterInfo[] parameters)
-        {
-            var middleware = typeof(T);
-            var actionArg = Expression.Parameter(typeof(IAction), "action");
-            var instanceArg = Expression.Parameter(middleware, "instance");
-
-            // Invoke method's argument is action only.
-            var methodArguments = new Expression[parameters.Length];
-            methodArguments[0] = actionArg;
-
-            Expression middleweareInstanceArg = instanceArg;
-            if (methodInfo.DeclaringType != typeof(T))
-            {
-                middleweareInstanceArg = Expression.Convert(middleweareInstanceArg, methodInfo.DeclaringType);
-            }
-
-            var body = Expression.Call(middleweareInstanceArg, methodInfo, methodArguments);
-
-            var lambda = Expression.Lambda<Func<T, IAction, IAction>>(body, instanceArg, actionArg);
-
-            return lambda.Compile();
+            return UseMiddleware(MiddlewareFactory.Create<TState>(middleware, args));
         }
     }
 }
