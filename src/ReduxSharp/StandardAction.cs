09e6b1db90783fca89b52835cb30dba3d048a825
tnakamura <griefworker@gmail.com>
2016-11-04 09:13:12 +0900

    [fix #1] Create 'StandardAction'

---
+++ src/ReduxSharp/StandardAction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReduxSharp
+{
+    /// <summary>
+    /// ReduxSharp's built in action type.
+    /// </summary>
+    public class StandardAction : IAction
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="StandardAction"/> class.
+        /// </summary>
+        /// <param name="type">String representation of the Action type</param>
+        /// <param name="payload">Payload convertable to JSON</param>
+        public StandardAction(string type, IDictionary<string, object> payload = null)
+        {
+            if (type == null) throw new ArgumentNullException(nameof(type));
+
+            Type = type;
+            Payload = payload;
+        }
+
+        /// <summary>
+        /// A string that identifies the type of this <see cref="StandardAction"/>
+        /// </summary>
+        public string Type { get; }
+
+        /// <summary>
+        /// An untyped, JSON-compatible payload
+        /// </summary>
+        public IDictionary<string, object> Payload { get; }
+    }
+}
---
+++ test/ReduxSharp.Tests/StandardActionTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ReduxSharp.Tests
+{
+    public class StandardActionTest
+    {
+        [Fact]
+        public void Constructor_initialize_new_instance()
+        {
+            var action = new StandardAction("TEST");
+            Assert.NotNull(action);
+            Assert.Equal("TEST", action.Type);
+            Assert.Null(action.Payload);
+        }
+
+        [Fact]
+        public void Constructor_initialize_new_instance_with_payload()
+        {
+            var action = new StandardAction("TEST", new Dictionary<string, object>()
+            {
+                ["FOO"] = "BAR",
+            });
+            Assert.NotNull(action);
+            Assert.Equal("TEST", action.Type);
+            Assert.NotNull(action.Payload);
+            Assert.Equal("BAR", action.Payload["FOO"]);
+        }
+
+        [Fact]
+        public void Constructor_throws_ArgumentNullException_when_type_is_null()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new StandardAction(null);
+            });
+        }
+    }
+}
