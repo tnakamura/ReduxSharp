4c4eae99e6cf122959349e7e87164567f9f96a9b
tnakamura <griefworker@gmail.com>
2018-05-17 17:46:25 +0900

    Added operators.

---
+++ src/ReduxSharp/Internal/SelectObservable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReduxSharp.Internal
+{
+    internal class SelectObservable<T, TResult> : IObservable<TResult>
+    {
+        readonly IObservable<T> source;
+
+        readonly Func<T, TResult> selector;
+
+        public SelectObservable(IObservable<T> source, Func<T, TResult> selector)
+        {
+            this.source = source;
+            this.selector = selector;
+        }
+
+        public IDisposable Subscribe(IObserver<TResult> observer)
+        {
+            return source.Subscribe(new Select(this, observer));
+        }
+
+        class Select : IObserver<T>
+        {
+            readonly SelectObservable<T, TResult> parent;
+
+            readonly IObserver<TResult> observer;
+
+            public Select(SelectObservable<T, TResult> parent, IObserver<TResult> observer)
+            {
+                this.parent = parent;
+                this.observer = observer;
+            }
+
+            public void OnCompleted()
+            {
+                observer.OnCompleted();
+            }
+
+            public void OnError(Exception error)
+            {
+                observer.OnError(error);
+            }
+
+            public void OnNext(T value)
+            {
+                observer.OnNext(parent.selector(value));
+            }
+        }
+    }
+}
---
+++ src/ReduxSharp/StoreExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReduxSharp.Internal;
+
+namespace ReduxSharp
+{
+    public static class StoreExtensions
+    {
+        public static IObservable<T> DistinctUntilChanged<T>(this IObservable<T> source)
+        {
+            return new DistinctUntilChangedObservable<T>(source, EqualityComparer<T>.Default);
+        }
+
+        public static IObservable<T> DistinctUntilChanged<T>(this IObservable<T> source, IEqualityComparer<T> comparer)
+        {
+            return new DistinctUntilChangedObservable<T>(source, comparer);
+        }
+
+        public static IObservable<TResult> Select<T, TResult>(this IObservable<T> source, Func<T, TResult> selector)
+        {
+            return new SelectObservable<T, TResult>(source, selector);
+        }
+    }
+}
