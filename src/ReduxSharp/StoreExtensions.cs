4cfcd4238850ee1975757c51a407d14d2c6a3505
tnakamura <griefworker@gmail.com>
2023-04-13 16:43:09 +0900

    Added StoreExtensions

---
+++ src/ReduxSharp/StoreExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ReduxSharp
+{
+    /// <summary>
+    /// Create an asynchronous action.
+    /// </summary>
+    /// <typeparam name="TState">A type of root state tree</typeparam>
+    /// <param name="store">A store</param>
+    /// <returns>The <see cref="Task"/> that represents the asynchronous operation.</returns>
+    public delegate Task AsyncActionCreator<TState>(IStore<TState> store);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class StoreExtensions
+    {
+        /// <summary>
+        /// Dispatches an asynchronous action.
+        /// </summary>
+        /// <typeparam name="TState">A type of root state tree</typeparam>
+        /// <param name="store">A store</param>
+        /// <param name="asyncActionCreator"></param>
+        /// <returns>The <see cref="Task"/> that represents the asynchronous operation.</returns>
+        public static async Task DispatchAsync<TState>(
+            this IStore<TState> store,
+            AsyncActionCreator<TState> asyncActionCreator)
+        {
+            if (asyncActionCreator == null) throw new ArgumentNullException(nameof(asyncActionCreator));
+            await asyncActionCreator(store).ConfigureAwait(false);
+        }
+    }
+}
